743a1195fa61fb0fd306bc165bf49caee5be1756
jecho-cruz <202312834@fit.edu.ph>
2024-07-12 02:14:26 +0800

    Add files via upload

---
+++ login-page.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+
+    if (username === 'admin' && password === 'admin') {
+
+        window.location.href = 'realfinal.html';
+    } else {
+        alert('Invalid username or password');
+    }
+});
